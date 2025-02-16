37ba9ef72fe06992cbf97ccaf72d8252d3a6ffc7
threader <michael.heltne@gmail.com>
2025-02-16 15:40:35 +0100

    legacy: make compatible with legacy devices note: $ANDROID_BUILD_TOP/development/vndk/tools/header-checker/utils/create_reference_dumps.py --lib libc --lib-variant LLNDK --release ap4a -p lineage_enchilada $ANDROID_BUILD_TOP/development/vndk/tools/header-checker/utils/create_reference_dumps.py --lib libc --lib-variant APEX --release ap4a -p lineage_enchilada
    Change-Id: I50bf22b1afca16d5809837ee816e445747ecfdb8

--- libc/bionic/malloc_common.h
+++ libc/bionic/malloc_common.h
@@ -57,9 +57,10 @@ __END_DECLS
 
 #ifdef  __LP64__
 #ifndef USE_H_MALLOC
-#error missing USE_H_MALLOC
+//#warning missing USE_H_MALLOC
 #endif
 
+#ifdef USE_H_MALLOC
 #include "h_malloc.h"
 #define Malloc(function)  h_ ## function
 __BEGIN_DECLS
@@ -73,12 +74,26 @@ void InitNativeAllocatorDispatch(libc_globals* globals);
 
 #define BOTH_H_MALLOC_AND_SCUDO
 
-#else // 32-bit
+#else
 #include "scudo.h"
 #define Malloc(function)  scudo_ ## function
 #endif
 
-#endif
+#endif // USE_H_MALLOC
+
+#if defined(USE_SCUDO)
+#include "scudo.h"
+#define Malloc(function)  scudo_ ## function
+
+#elif defined(USE_SCUDO_SVELTE)
+#include "scudo.h"
+#define Malloc(function)  scudo_svelte_ ## function
+#else
+#include "jemalloc.h"
+#define Malloc(function)  je_ ## function
+#endif // USE_SCUDO
+
+#endif //HW_ASAN
 
 const MallocDispatch* NativeAllocatorDispatch();
 
