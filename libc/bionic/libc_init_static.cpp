50c94cfd6c1e0473305bc3b43effe9e585c30bfd
threader <michael.heltne@gmail.com>
2025-04-09 10:30:27 +0200

    Revert "add sysprop for enabling heap memory tagging in vendor processes" - redo this sometime later for a lineage-*_hardend_mte branch
    This reverts commit 7df392a8ec33263bc9039ff500ff088858cd40ac.
    
    Change-Id: Ic0ad1e18a3300b7f41afe7f50fec675d97e702bc

--- libc/bionic/libc_init_static.cpp
+++ libc/bionic/libc_init_static.cpp
@@ -207,10 +207,6 @@ static HeapTaggingLevel __get_memtag_level_from_note(const ElfW(Phdr) * phdr_sta
   }
 }
 
-static bool starts_with(const char* s, const char* prefix) {
-    return strncmp(s, prefix, strlen(prefix)) == 0;
-}
-
 // Returns true if there's an environment setting (either sysprop or env var)
 // that should overwrite the ELF note, and places the equivalent heap tagging
 // level into *level.
@@ -222,20 +218,6 @@ static bool get_environment_memtag_setting(HeapTaggingLevel* level) {
 
   const char* progname = __libc_shared_globals()->init_progname;
   if (progname == nullptr) return false;
-
-  bool is_vendor_prog = starts_with(progname, "/vendor/") || starts_with(progname, "/apex/com.google.");
-  char prop_value[8];
-  if (is_vendor_prog && get_property_value("persist.arm64.memtag.vendor", prop_value, sizeof(prop_value))) {
-    if (strcmp("1", prop_value) == 0) {
-      *level = M_HEAP_TAGGING_LEVEL_ASYNC;
-      return true;
-    }
-  }
-
-  if (!is_debug_build) {
-      // ignore heap memory tagging setting overrides on user builds
-      return false;
-  }
 
   const char* basename = __gnu_basename(progname);
 
